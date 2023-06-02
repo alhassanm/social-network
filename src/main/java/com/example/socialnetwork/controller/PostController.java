58099e984866c465d5b402df983b98d24a48e063
Mahmoud Alhassan <mahmoud.k.alhassan@outlook.com>
2023-06-03 01:11:07 +0300

    update methodname in post controller

--- src/main/java/com/example/socialnetwork/controller/PostController.java
+++ src/main/java/com/example/socialnetwork/controller/PostController.java
@@ -1,10 +1,10 @@
 package com.example.socialnetwork.controller;
 
 import com.example.socialnetwork.dto.PostDTO;
-import com.example.socialnetwork.model.Post;
 import com.example.socialnetwork.service.PostService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,11 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/posts")
 
 public class PostController {
-    private PostService postService;
+    private final PostService postService;
     public PostController(PostService postService){
         this.postService = postService;
     }
-    public ResponseEntity<PostDTO> createPsot(
+    @PostMapping
+    public ResponseEntity<PostDTO> createPost(
             @RequestBody PostDTO postDTO
     ){
         return new ResponseEntity<>(
